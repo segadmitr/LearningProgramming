e93960aeec71d5f9dc2a009f339d9f3dd224259b
sega <segadmitr@yandex.ru>
2015-04-15 09:03:22 +0300

    лаба 1 таск 2 продолжение

--- MultiThread/Task3/MainWindow.xaml.cs
+++ MultiThread/Task3/MainWindow.xaml.cs
@@ -20,9 +20,62 @@ namespace Task3
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region private
+        
+        /// <summary>
+        /// Длины элементов
+        /// </summary>
+        readonly List<int> _lengthsElements = new List<int> { 10, 100, 1000, 100000 };
+        
+        /// <summary>
+        /// Количества потоков
+        /// </summary>
+        readonly List<int> _countsThreads = new List<int> { 2, 3, 4, 5, 10 };
+
+        /// <summary>
+        /// Сгенерированные значения элементов
+        /// </summary>
+        List<int> _generatedElements = new List<int>();
+        
+        #endregion
+        
         public MainWindow()
         {
             InitializeComponent();
+            generateElements(_lengthsElements.Max());
         }
+
+        /// <summary>
+        /// Генерирует элементы массива
+        /// </summary>
+        /// <param name="lenchElements"></param>
+        private  void generateElements(int lenchElements)
+        {
+            var rnd = new Random();
+            for (var i = 0; i < lenchElements; i++)
+            {
+                _generatedElements.Add(rnd.Next(0, 100));
+            }
+        }
+
+
+        private void calculate()
+        {
+            foreach (var length in _lengthsElements)
+            {
+                var elements = _generatedElements.Take(length);
+                foreach (var countThreads in _countsThreads)
+                {
+                }
+            }
+        }
+
+
+
+        private void RunBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+                
     }
 }
