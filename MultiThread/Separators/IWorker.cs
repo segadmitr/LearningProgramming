4a3c9cd46e0bb9a0e90941329de1c4c9e7110b6e
dmitriev-s <sdmitriev@cognitive.ru>
2015-04-27 15:05:08 +0400

    Начало, переделка обработчика потоков для сепаратора

--- MultiThread/Separators/IWorker.cs
+++ MultiThread/Separators/IWorker.cs
@@ -12,9 +12,18 @@ namespace Separators
     public interface IWorker<T>
     {
         /// <summary>
-        /// Расчитать
+        /// Рассчитать и модифицировать элемент списка
         /// </summary>
+        /// <param name="calculatedParam">Все данные</param>
+        /// <param name="doWithParamItem">Модификатор элемента списка</param>
         void Calculate(List<T> calculatedParam, Func<T,int,T> doWithParamItem);
+        
+        /// <summary>
+        /// Рассчитать разделенную часть данных
+        /// </summary>
+        /// <param name="calculatedParam">Все данные</param>
+        /// <param name="doWithSeparatedPart">Разделенная часть данных</param>
+        void Calculate(List<T> calculatedParam, Func<IEnumerable<int>, IEnumerable<T>> doWithSeparatedPart);
 
         /// <summary>
         /// Количество потоков 
@@ -34,35 +43,27 @@ namespace Separators
     public class Worker<T> : IWorker<T>
     {
         List<T> _calculatedParam = new List<T>();
-        Func<T,int, T> _doWithParamItem;
         int _countThreads;
 
+        #region Модификация элемента списка
+        
+        /// <summary>
+        /// Делегат обработки элемента
+        /// </summary>
+        Func<T, int, T> _doWithParamItem;
+
         //TODO List<T> calculatedParam переделать на IEnumerable
-        public void Calculate(List<T> calculatedParam, Func<T,int,T> doWithParamItem)
+        public void Calculate(List<T> calculatedParam, Func<T, int, T> doWithParamItem)
         {
             _calculatedParam = calculatedParam;
             _doWithParamItem = doWithParamItem;
-            calculate(_calculatedParam.Count(), CountThreads);
-        }
-
-        private void calculate(int lenchElements, int countThreads)
-        {
-            var separateResult = Separator.Separate(lenchElements, countThreads);
-            var threadList = new List<Thread>();
-            foreach (var sepItemResult in separateResult)
-            {
-                var thread = new Thread(threadWorking);
-                threadList.Add(thread);
-                thread.Start(sepItemResult);
-            }
-
-            threadList.ForEach(s => s.Join());
+            calculate(_calculatedParam.Count(), CountThreads, threadWorkForModItem);
         }
 
         /// <summary>
-        /// обработка для одного потока
+        /// Модификация элементов для одного потока
         /// </summary>
-        private void threadWorking(object indexesParam)
+        private void threadWorkForModItem(object indexesParam)
         {
             var indexes = indexesParam as IEnumerable<int>;
             if (indexes == null)
@@ -72,6 +73,47 @@ namespace Separators
             {
                 _calculatedParam[itemIndex] = _doWithParamItem(_calculatedParam[itemIndex], itemIndex);
             }
+        }
+
+        #endregion
+
+        #region Расчет части  списка, разделеннного делителем
+
+        Func<IEnumerable<int>, IEnumerable<T>> _doWithSeparatedPart;
+
+        public void Calculate(List<T> calculatedParam, Func<IEnumerable<int>, IEnumerable<T>> doWithSeparatedPart)
+        {
+            _calculatedParam = calculatedParam;
+            _doWithSeparatedPart = doWithSeparatedPart;
+            calculate(_calculatedParam.Count(), CountThreads, threadWorkForSepList);
+        }
+
+        /// <summary>
+        /// Обработка элементов одного потока
+        /// </summary>
+        /// <param name="indexesParam"></param>
+        void threadWorkForSepList(object indexesParam)
+        {
+            var indexes = indexesParam as IEnumerable<int>;
+            if (indexes == null)
+                throw new ArgumentException();
+            var d = _doWithSeparatedPart(indexes);
+        }
+
+        #endregion
+
+        private void calculate(int lenchElements, int countThreads, ParameterizedThreadStart threadStartMethod)
+        {
+            var separateResult = Separator.Separate(lenchElements, countThreads);
+            var threadList = new List<Thread>();
+            foreach (var sepItemResult in separateResult)
+            {
+                var thread = new Thread(threadStartMethod);
+                threadList.Add(thread);
+                thread.Start(sepItemResult);
+            }
+
+            threadList.ForEach(s => s.Join());
         }
 
         public int CountThreads
